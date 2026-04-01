d69ca71629777214f6c8ca692a4e876b6f90915f
xiyosenstore <ziyosenstore@gmail.com>
2026-04-02 02:49:33 +0700

    Update route.ts

--- src/app/api/dramabox/foryou/route.ts
+++ src/app/api/dramabox/foryou/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 
-const UPSTREAM_API = process.env.UPSTREAM_API || "https://api.sansekai.my.id/api/netshort/foryou";
+const UPSTREAM_API = process.env.UPSTREAM_API || "https://api.sansekai.my.id/api/dramabox/foryou";
 
 export async function GET() {
   try {
