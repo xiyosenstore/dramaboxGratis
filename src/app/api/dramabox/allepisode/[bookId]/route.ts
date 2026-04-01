ca623bc53f66c7a4496671bc703b3541ed5fa5cd
xiyosenstore <ziyosenstore@gmail.com>
2026-04-02 01:03:14 +0700

    Update UPSTREAM_API URL in route.ts

--- src/app/api/dramabox/allepisode/[bookId]/route.ts
+++ src/app/api/dramabox/allepisode/[bookId]/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import { headers } from "next/headers";
 
-const UPSTREAM_API = process.env.UPSTREAM_API || "https://dramabox.dramabos.my.id";
+const UPSTREAM_API = process.env.UPSTREAM_API || "https://dramabox.dramabos.my.id/api.html";
 
 export async function GET(
   request: NextRequest,
