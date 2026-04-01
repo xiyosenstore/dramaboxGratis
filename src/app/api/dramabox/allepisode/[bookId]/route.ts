6d81d050311d791ce12ed6c4b2a5dee50be8ad5c
xiyosenstore <ziyosenstore@gmail.com>
2026-04-02 02:22:28 +0700

    Update route.ts

--- src/app/api/dramabox/allepisode/[bookId]/route.ts
+++ src/app/api/dramabox/allepisode/[bookId]/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import { headers } from "next/headers";
 
-const UPSTREAM_API = process.env.UPSTREAM_API || "https://dramabox.dramabos.my.id/api/v1/allepisode?bookId=42000008315&lang=in&code=Qryijg";
+const UPSTREAM_API = process.env.UPSTREAM_API || "https://netshort.dramabos.my.id/api/home/1?lang=in";
 
 export async function GET(
   request: NextRequest,
